3cca7da2c6c749d4bdcf407c1a69950e6f762f20
Mohammed Junaid <juna0006@algonquinlive.com>
2023-01-24 22:12:22 -0500

    java

--- app/src/main/java/algonquin/cst2335/juna0006/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/juna0006/MainActivity.java
@@ -3,6 +3,8 @@ package algonquin.cst2335.juna0006;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        TextView tv = findViewById(R.id.textHelloWorld);
     }
 }
