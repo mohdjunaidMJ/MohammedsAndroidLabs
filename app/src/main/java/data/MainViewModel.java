97b6e0aefa310f7a194a4d689542b5c389108a4e
Mohammed Junaid <juna0006@algonquinlive.com>
2023-01-31 14:32:49 -0500

    adding buttons

--- app/src/main/java/data/MainViewModel.java
+++ app/src/main/java/data/MainViewModel.java
@@ -1,7 +1,10 @@
 package data;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
     public String editString;
+    public MutableLiveData<Boolean> isSelected;
 }
