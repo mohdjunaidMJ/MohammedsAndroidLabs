4efe3b9cc0dfb0905d616dc68cc1a4e062f87fa2
Mohammed Junaid <juna0006@algonquinlive.com>
2023-03-29 19:02:01 -0400

    Finished week6

---
+++ app/src/main/java/algonquin/cst2335/juna0006/ChatRoomViewModel.java
@@ -0,0 +1,24 @@
+package algonquin.cst2335.juna0006;
+
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class ChatRoomViewModel extends ViewModel {
+    public ArrayList<ChatMessage> messageList = new ArrayList<>();
+
+}
+
+
+
+//import androidx.lifecycle.ViewModel;
+
+//import java.util.ArrayList;
+
+//import algonquin.cst2335.juna0006.ChatMessage;
+
+   // public class ChatRoomViewModel extends ViewModel {
+  //      public ArrayList<ChatMessage> messageList = new ArrayList<>();
+
+   // }
+//}
